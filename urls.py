0ecdf4c40127679a0fca210a90fe1e548aaa6961
tcarobruce <tcarobruce@gmail.com>
2015-01-29 16:28:10 -0800

    Fix for Django 1.7, retab

--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'', include('boundaries.urls')),
-	(r'', include('maps.urls')),
-	
+    (r'', include('boundaries.urls')),
+    (r'', include('maps.urls')),
+
     # Examples:
     # url(r'^$', 'boundaries_us.views.home', name='home'),
     # url(r'^boundaries_us/', include('boundaries_us.foo.urls')),
